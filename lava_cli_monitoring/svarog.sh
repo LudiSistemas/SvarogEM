188cb55b46c85f75649ba7f71c12adb6fe61cfcb
Ludi Sistemas <portalscg@gmail.com>
2023-11-05 15:47:17 +0100

    fixing order of message to display

--- lava_cli_monitoring/svarog.sh
+++ lava_cli_monitoring/svarog.sh
@@ -14,11 +14,11 @@ declare -A processed_events
 IFS=',' read -r -a event_names <<< "$event_name"
 for en in "${event_names[@]}"; do
     if [ "$en" == "lava_provider_jailed" ]; then
-        parse_and_display_jailed_events "$en"
         echo "starting with $en"
+        parse_and_display_jailed_events "$en" 
     elif [ "$en" == "lava_freeze_provider" ]; then
-        parse_and_display_freeze_events "$en"
         echo "starting with $en"
+        parse_and_display_freeze_events "$en"
     else
         echo "Unsupported event name: $en"
     fi

8452c1cea9ccc784382030e3656a2d67292ee3bb
Ludi Sistemas <portalscg@gmail.com>
2023-11-05 17:39:52 +0100

    modifing the svarog

--- lava_cli_monitoring/svarog.sh
+++ lava_cli_monitoring/svarog.sh
@@ -7,20 +7,27 @@ else
     exit 1
 fi
  source functions.sh
-
-
+declare -A processed_events
 
 
 IFS=',' read -r -a event_names <<< "$event_name"
 for en in "${event_names[@]}"; do
     if [ "$en" == "lava_provider_jailed" ]; then
-        declare -A processed_events
         echo "starting with $en"
         parse_and_display_jailed_events "$en" 
+
     elif [ "$en" == "lava_freeze_provider" ]; then
-        declare -A processed_events
         echo "starting with $en"
         parse_and_display_freeze_events "$en"
+
+    elif [ "$en" == "lava_unfreeze_provider" ]; then
+        echo "starting with $en"
+        parse_and_display_unfreeze_events "$en"
+
+    elif [ "$en" == "lava_stake_new_provider" ]; then
+        echo "starting with $en"
+        parse_and_display_new_stake_events "$en"
+        
     else
         echo "Unsupported event name: $en"
     fi

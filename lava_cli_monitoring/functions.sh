99b673a6c1541cb58ef94f0e895ecc88746c8b76
Ludi Sistemas <portalscg@gmail.com>
2023-11-07 21:16:27 +0100

    trying difrent aproach because of faulty time

--- lava_cli_monitoring/functions.sh
+++ lava_cli_monitoring/functions.sh
@@ -43,91 +43,60 @@ is_provider_monitored() {
     jq -e --arg provider "$provider" '.[] | select(.wallet == $provider)' monitored2.json >/dev/null
 }
 
+skip_to_jailed_events() {
+    while read -r line; do
+        if [[ "$line" == *"lava_provider_jailed"* ]]; then
+            # Found the first jailed event line, break the loop to continue processing.
+            break
+        fi
+    done < <(run_lavad_command)
+}
+
 parse_and_display_jailed_events() {
     local event_name=$1
     local output
+    local event_found=false
+
+    # Run the lavad command and capture its output.
     if ! output=$(run_lavad_command); then
-      return 1
+        echo "Failed to get output from lavad command."
+        return 1
     fi
 
+    # Check if the event name is correct.
     if [ "$event_name" != "lava_provider_jailed" ]; then
         echo "Event '$event_name' is not 'lava_provider_jailed'. Skipping..."
         return 0
     fi
 
-    local count=$(echo "$output" | grep -c "lava_provider_jailed")
+    # Read through each line of the output.
+    echo "$output" | while IFS= read -r line; do
+        # If we find a line with "Current Block", we skip it.
+        if echo "$line" | grep -q "Current Block:"; then
+            continue
+        fi
 
-    if [ $count -eq 0 ]; then
-        echo "No 'lava_provider_jailed' events found in the last output."
-        return 0
-    fi
+        # Check for the "lava_provider_jailed" event.
+        if echo "$line" | grep -q "lava_provider_jailed"; then
+            event_found=true
 
-    echo "$output" | grep "lava_provider_jailed" | while read -r line; do
-    date_time=""
-    local date_time=$(echo "$line" | awk '{print $1, $2, $3}')
-    local provider=$(echo "$line" | awk -F'provider_address =' '{print $2}' | awk '{print $1}' | tr -d ',')
-    local chain_id=$(echo "$line" | awk -F'chain_id = ' '{print $2}' | awk '{print $1}')
-    local complaint_cu=$(echo "$line" | awk -F'complaint_cu = ' '{print $2}' | awk '{print $1}')
-    local height=$(echo "$line" | awk -F'height=' '{print $2}' | awk '{print $1}')
-    event_time=$(echo "$line" | grep -oP 'Event Time: \K.*?(?=Provider:)' | sed 's/Current Block: //')
-
-        local event_key="${provider}_${chain_id}"
-        
-        
-
-        if [[ -z ${processed_events[$event_key]} ]]; then
-            processed_events[$event_key]=1
-
-            if is_provider_monitored "$provider"; then
+            local date_time=$(echo "$line" | awk '{print $1, $2, $3}')
+            local provider=$(echo "$line" | awk -F'provider_address =' '{print $2}' | awk '{print $1}' | tr -d ',')
+            local chain_id=$(echo "$line" | awk -F'chain_id = ' '{print $2}' | awk '{print $1}')
+            local complaint_cu=$(echo "$line" | awk -F'complaint_cu = ' '{print $2}' | awk '{print $1}')
+            local height=$(echo "$line" | awk -F'height=' '{print $2}' | awk '{print $1}')
             
-            if [ "$USE_TELEGRAM" = true ]; then
-                local provider_link="[$provider](https://info.lavanet.xyz/provider/$provider)"
-                local provider_name=$(jq -r --arg provider "$provider" '.[] | select(.wallet == $provider) | .name' monitored2.json)
-                local telegram_message="----------------------------------------%0A"
-                telegram_message+="Provider jailed event detected for $provider_name%0A"
-                telegram_message+="Event Time: $date_time%0A"
-                telegram_message+="Provider: $provider_link%0A"
-                telegram_message+="Chain ID: $chain_id%0A"
-                telegram_message+="Complaint CU: $complaint_cu%0A"
-                telegram_message+="Height: $height%0A"
-                telegram_message+="----------------------------------------"
-
-                send_telegram_message "$telegram_message"
-
-            elif [ "$USE_SLACK" = true ]; then
-                local provider_link="<https://info.lavanet.xyz/provider/$provider|$provider>"
-                local provider_name=$(jq -r --arg provider "$provider" '.[] | select(.wallet == $provider) | .name' monitored2.json)
-                local slack_message="{
-                    \"text\": \"Provider jailed event detected for $provider_name\",
-                    \"attachments\": [
-                        {
-                            \"fields\": [
-                                { \"title\": \"Event Time\", \"value\": \"$date_time\", \"short\": true },
-                                { \"title\": \"Provider\", \"value\": \"$provider_link\", \"short\": true },
-                                { \"title\": \"Chain ID\", \"value\": \"$chain_id\", \"short\": true },
-                                { \"title\": \"Complaint CU\", \"value\": \"$complaint_cu\", \"short\": true },
-                                { \"title\": \"Height\", \"value\": \"$height\", \"short\": true }
-                            ],
-                            \"color\": \"#FF5733\"
-                        }
-                    ]
-                }"
-
-                send_slack_message "$slack_message"
-
-            else
-            echo "No communication channel avilable"
-            fi
-
-            
-            else
-                echo "Provider $provider is not on the monitored list."
-            fi
-        else
-            echo "Event for provider $provider on chain ID $chain_id has already been processed."
+            # ... (rest of your existing logic to handle the event)
         fi
     done
+
+    # If no events were found, we output a message.
+    if ! $event_found; then
+        echo "No 'lava_provider_jailed' events found."
+    fi
 }
+
+
 parse_and_display_freeze_events() {
     local event_name=$1
     local output
